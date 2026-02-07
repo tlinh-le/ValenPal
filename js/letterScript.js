fdac0139c828eb50c90a473da5b7ffe2bc5ef401
tlinh-le <linhthule1910@gmail.com>
2026-02-07 18:33:59 -0500

    add changes

--- js/letterScript.js
+++ js/letterScript.js
@@ -1,8 +1,39 @@
 'use strict';
 
-const stickers = document.querySelector('.sticker');
+let newX = 0;
+let newY = 0;
+let startX = 0;
+let startY = 0;
+
+/*const stickers = document.querySelector('.sticker');
 for(let i = 0; i < stickers.length; i++){
-    stickers[i].addEventListener('click', clone);
+    stickers[i].addEventListener('mousedown', mouseDown);
+}*/
+
+const sticker = document.getElementById('sticker');
+sticker.addEventListener('mousedown', mouseDown);
+
+function mouseDown(event){
+    startX = event.clientX;
+    startY = event.clientY; 
+
+    document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('mouseup', mouseUp);
+}
+
+function mouseMove(event){
+    newX = startX - event.clientX;
+    newY = startY - event.clientY;
+
+    startX = event.clientX;
+    startY = event.clientY;
+
+    sticker.style.top = (sticker.offsetTop - newY) + 'px';
+    sticker.style.left = (sticker.offsetTop - newX) + 'px';
+}
+
+function mouseUp(event){
+    document.removeEventListener('mousemove', mouseMove);
 }
 
 // clone sticker as a child in .letter
